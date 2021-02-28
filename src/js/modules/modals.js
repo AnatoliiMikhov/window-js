5fe81d542c7764eeeb57a8fbc3e0a58febed8195
AnatoliiMikhov <anatoli.mihov@gmail.com>
2021-02-28 20:58:49 +0200

    tabs script and tabs content animated

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -13,21 +13,18 @@ const modals = () => {
 
 				modal.style.display = 'block';
 				document.body.style.overflow = 'hidden';
-				// document.body.classList.add('modal-open');
 			});
 		})
 
 		close.addEventListener('click', () => {
 			modal.style.display = 'none';
 			document.body.style.overflow = '';
-			// document.body.classList.remove('modal-open');
 		});
 
 		modal.addEventListener('click', (e) => {
 			if (e.target === modal) {
 				modal.style.display = 'none';
 				document.body.style.overflow = '';
-				// document.body.classList.remove('modal-open');
 			}
 		});
 	}
@@ -41,7 +38,7 @@ const modals = () => {
 
 	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
 	bindModal('.phone_link', '.popup', '.popup .popup_close');
-	showModalByTime('.popup', 3000);
+	// showModalByTime('.popup', 6000);
 };
 
 export default modals;
