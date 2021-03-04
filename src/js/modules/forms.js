5e7cd95460a0e30d5c1d8ef9eaa46387a630e601
AnatoliiMikhov <anatoli.mihov@gmail.com>
2021-03-04 13:39:41 +0200

    tabs forms modals

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,15 +1,12 @@
-const forms = () => {
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
 	const form = document.querySelectorAll('form'),
-		inputs = document.querySelectorAll('input'),
-		phoneInputs = document.querySelectorAll('input[name=user_phone]');
+		inputs = document.querySelectorAll('input');
 
 	/* ------------------------------- check input ------------------------------ */
 
-	phoneInputs.forEach(item => {
-		item.addEventListener('input', () => {
-			item.value = item.value.replace(/\D/, '');
-		});
-	});
+	checkNumInputs('input[name=user_phone]');
 
 	/* -------------------------------- messages -------------------------------- */
 
@@ -50,6 +47,11 @@ const forms = () => {
 			item.appendChild(statusMessage);
 
 			const formData = new FormData(item);
+			if (item.getAttribute('data-calc') === 'end') {
+				for (let key in state) {
+					formData.append(key, state[key]);
+				}
+			}
 
 			postData('assets/server.php', formData)
 				.then(res => {
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,9 +1,25 @@
 const modals = () => {
-	function bindModal(triggerSelector, modalSelector, closeSelector) {
+	function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
 
 		const trigger = document.querySelectorAll(triggerSelector),
 			modal = document.querySelector(modalSelector),
-			close = document.querySelector(closeSelector);
+			close = document.querySelector(closeSelector),
+			windows = document.querySelectorAll('[data-modal]');
+
+/* -------------------------------- functions ------------------------------- */
+
+		const hideModals = () => {
+			windows.forEach(item => {
+				item.style.display = 'none';
+			});
+		};
+
+		const toggleModals = (display, overflow) => {
+			modal.style.display = display;
+			document.body.style.overflow = overflow;
+		};
+
+/* --------------------------------- events --------------------------------- */
 
 		trigger.forEach(item => {
 			item.addEventListener('click', (e) => {
@@ -11,25 +27,27 @@ const modals = () => {
 					e.preventDefault();
 				}
 
-				modal.style.display = 'block';
-				document.body.style.overflow = 'hidden';
+				hideModals();
+				toggleModals('block', 'hidden');
 			});
 		})
 
 		close.addEventListener('click', () => {
-			modal.style.display = 'none';
-			document.body.style.overflow = '';
+
+			hideModals();
+			toggleModals('none', '');
 		});
 
 		modal.addEventListener('click', (e) => {
-			if (e.target === modal) {
-				modal.style.display = 'none';
-				document.body.style.overflow = '';
+			if (e.target === modal && closeClickOverlay) {
+
+				hideModals();
+				toggleModals('none', '');
 			}
 		});
 	}
 
-	function showModalByTime(selector, time){
+	function showModalByTime(selector, time) {
 		setTimeout(() => {
 			document.querySelector(selector).style.display = 'block';
 			document.body.style.overflow = 'hidden';
@@ -38,6 +56,9 @@ const modals = () => {
 
 	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
 	bindModal('.phone_link', '.popup', '.popup .popup_close');
+	bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+	bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+	bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
 	// showModalByTime('.popup', 6000);
 };
 
